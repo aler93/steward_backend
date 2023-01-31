e6833dcc24404dc629c177fa367363d33146b91d
Alisson Naimayer <anaimayer3@gmail.com>
2023-01-31 11:23:36 -0300

    asdasdsadsa

--- app/Http/Controllers/Saude/UsuarioController.php
+++ app/Http/Controllers/Saude/UsuarioController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers\Saude;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Exception;
 
 class UsuarioController extends Controller
 {
-    //
+    public function estatisticas(string $uuidUser) 
+    {
+        try {
+            $dados = [];
+
+            return $this->json(["estatisticas" => $dados]);
+        } catch( Exception $e ) {
+            return $this->jsonException($e);
+        }
+    }
 }
